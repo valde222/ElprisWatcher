dd3a7ec77d05d39ad871fc458f89fa3f3e3214fa
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2022-12-14 10:02:18 +0100

    refactored the dataretriever, and made some comments

--- src/main/java/dataRetriever.java
+++ src/main/java/dataRetriever.java
@@ -5,35 +5,61 @@ import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public class dataRetriever {
-    public void retrieveData() throws IOException, ParseException {
-        String urlString =  URLGenerator("2022-12-06", "2022-12-08");
-        URL url = new URL(urlString);
-        URLConnection connect = url.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
+    public void retrieveData() throws IOException, ParseException {
+        // Generate the URL for the energy data service
+        String urlString = generateEnergyDataServiceURL("2022-12-06", "2022-12-08", "HourDK", "HourDK,SpotPriceDKK", "{\"PriceArea\":[\"DK2\"]}");
+
+        // Open a connection to the URL and read the response
+        URL url = new URL(urlString);
+        InputStream inputStream = url.openConnection().getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        // Concatenate the lines from the response into a single string
+        String response = reader.lines().collect(Collectors.joining());
+
+        // Parse the response into a JSONObject and get the "records" JSONArray from it
         JSONParser parser = new JSONParser();
-        JSONObject responseJSONObject = (JSONObject) parser.parse(response.toString());
-        JSONArray recordsJSONArray = (JSONArray) responseJSONObject.get("records");
-        for (int i = 0; i < recordsJSONArray.size(); i++) {
-            JSONObject record = (JSONObject) recordsJSONArray.get(i);
-            System.out.println(record.get("HourDK"));
-            System.out.println(record.get("SpotPriceDKK"));
+        JSONObject json = (JSONObject) parser.parse(response);
+        JSONArray records = (JSONArray) json.get("records");
+
+        Map<String, Double> data = new HashMap<>();
+
+        for (Object JSONObject : records) {
+            JSONObject record = (JSONObject) JSONObject;
+
+            // Get the HourDK and SpotPriceDKK values from each object in records
+            String hourDK = (String) record.get("HourDK");
+            double spotPriceDKK = (double) record.get("SpotPriceDKK");
+
+            // Store the values in the HashMap
+            data.put(hourDK, spotPriceDKK / 1000);
         }
-        System.out.println("Slut");
+        System.out.println(data);
     }
-    public String URLGenerator(String includedStartDate, String notIncludedEndDate) {
-        String URLString = "https://api.energidataservice.dk/dataset/Elspotprices?" + "start=" + includedStartDate + "&end=" + notIncludedEndDate + "&sort=HourDK&columns=HourDK,PriceArea,SpotPriceDKK&filter={\"PriceArea\":[\"DK2\"]}";
-        return URLString;
+
+    public String generateEnergyDataServiceURL(String startDate, String endDate, String sortBy, String columns, String filter) {
+        // Create a new StringJoiner to build the URL string
+        StringJoiner urlJoiner = new StringJoiner("&", "https://api.energidataservice.dk/dataset/Elspotprices?", "");
+
+        // Add each query parameter to the URL string
+        urlJoiner.add("start=" + startDate);
+        urlJoiner.add("end=" + endDate);
+        urlJoiner.add("sort=" + sortBy);
+        urlJoiner.add("columns=" + columns);
+        urlJoiner.add("filter=" + filter);
+
+        // Return the generated URL string
+        System.out.println(urlJoiner.toString());
+        return urlJoiner.toString();
     }
 }
